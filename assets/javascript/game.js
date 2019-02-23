ab37bfdc3f442751c58b2fd415f211d59f9e79f0
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-02-22 22:51:07 -0500

    rejigger of js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,81 +3,131 @@ var wordBank =
     "sourdough", "brioche", "marbled rye", "cornbread", "ciabatta", "focaccia", "lavash", "pumpernickel", "baguette", "boule", "dampfnudel",
   ];
 
-const maxTries = 10;
+
+//letters tried
 var lettersTried = [];
+//index of chosen word form wordbank
 var activeWordIndex;
-var activeWord = [];
-var guessesLeft = 0;
+//array of the active word
+var activeWord;
+// guesses left
+var guessesLeft = 10;
+var guessArray = [];
 var gameStart = false;
 var finish = false;
 var wins = 0;
 
 
 function gameRestart() {
-  guessesLeft = maxTries;
+  guessesLeft = 10;
   activeWordIndex = Math.floor(Math.random() * (wordBank.length));
   lettersTried = [];
-  activeWord = [];
+  activeWord = wordBank[activeWordIndex].split("");
+  document.addEventListener("keydown", getUserGuess);
+  initGuessArray()
+};
 
-  document.getElementById("#").src = "";
+function initGuessArray() {
+  for (var i = 0; i < activeWord.length; i++) {
+    var letterObject = {
+      letter: activeWord[i],
+      isGuessed: false
+    }
 
+    guessArray.push(letterObject);
+  }
+}
 
-
-
-  document.onkeydown = function (event) {
-    if (finish) {
-      gameRestart();
-      finish = false;
+function getUserGuess(event) {
+  if (event.keyCode >= 65 && event.keyCode <= 90) {
+    // repeat key press indication 
+    if (lettersTried.includes(event.key)) {
+      console.log("poopFace")
     } else {
-      if (event.keyCode >= 65 && event.keyCode <= 90) {
-        guessSelect(event.key.toLowerCase());
+      // letters tried push into previously guessed array
+      lettersTried.push(event.key.toLowerCase())
+      guessTest(event.key.toLowerCase());
+    }
+  }
+};
+
+
+function guessTest(letter) {
+  // return an array of letters from the guess array 
+  var arr = guessArray.map(item => item.letter)
+  console.log(arr)
+  if (arr.includes(letter)) {
+    for (var i = 0; i < guessArray.length; i++) {
+      console.log(guessArray[i])
+      console.log(lettersTried)
+      if (guessArray[i].letter === letter) {
+        guessArray[i].isGuessed = true
+        console.log(guessArray[i].isGuessed)
       }
     }
-  };
+  } else {
+    guessesLeft--
+    console.log(guessesLeft)
+  }
 
-  function guessSelect(letter) {
-    if (guessesLeft > 0) {
-      if (!gameStarted) {
-        gameStarted = true;
-      }
+};
 
-      if (lettersTried.indexOf(letter) === -1) {
-        lettersTried.push(letter);
-        guessTest(letter);
-      }
-    }
-
-    updateDisplay();
-    checkWin();
-  };
+gameRestart();
 
 
-  function guessTest(letter) {
-    var letterStorage = [];
 
-    for (var i = 0; i < wordBank[activeWordIndex].length; i++) {
-      if (wordBank[activeWordIndex][i] === letter) {
-        letterStorage.push(i);
-      }
-    }
+// document.onkeydown = function (event) {
+//   if (finish) {
+//     gameRestart();
+//     finish = false;
+//   } else {
+//    
+//   }
+// };
+
+// function guessSelect(letter) {
+//   if (guessesLeft > 0) {
+//     if (!gameStarted) {
+//       gameStarted = true;
+//     }
+
+//     if (lettersTried.indexOf(letter) === -1) {
+//       lettersTried.push(letter);
+//       guessTest(letter);
+//     }
+//   }
+
+//   updateDisplay();
+//   checkWin();
+// };
 
 
-    if (letterStorage.length <= 0) {
-      guessesLeft--;
+// function guessTest(letter) {
+//   var letterStorage = [];
 
-    } else {
+//   for (var i = 0; i < wordBank[activeWordIndex].length; i++) {
+//     if (wordBank[activeWordIndex][i] === letter) {
+//       letterStorage.push(i);
+//     }
+//   }
 
-      for (var i = 0; i < letterStorage.length; i++) {
-        activeWord[letterStorage[i]] = letter;
-      }
-    }
-  };
 
-  function checkWin() {
-    if (activeWord.indexOf("_") === -1) {
-      document.getElementById("winner-image").style.cssText = "display: block";
-      document.getElementById("pressKeyTryAgain").style.cssText = "display: block";
-      wins++;
-      finish = true;
-    }
-  };
+//   if (letterStorage.length <= 0) {
+//     guessesLeft--;
+
+//   } else {
+
+//     for (var i = 0; i < letterStorage.length; i++) {
+//       activeWord[letterStorage[i]] = letter;
+//     }
+//   }
+// };
+
+// function checkWin() {
+//   if (activeWord.indexOf("_") === -1) {
+//     document.getElementById("winner-image").style.cssText = "display: block";
+//     document.getElementById("pressKeyTryAgain").style.cssText = "display: block";
+//     wins++;
+//     finish = true;
+//   }
+// };
