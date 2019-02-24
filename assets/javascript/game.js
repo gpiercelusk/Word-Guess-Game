3a0dfbf4b59f18966c2e7eeeecb98299ce01b93a
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-02-24 18:48:12 -0500

    moar style changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ var wordBank = ["sourdough", "brioche", "marbled rye", "cornbread", "ciabatta", 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
+var guessesLeft = 8;
 var lettersTried = [];
 var userGuesses = null;
 var activeWord = wordBank[Math.floor(Math.random() * wordBank.length)];
@@ -34,7 +34,7 @@ function consoleLogger() {
 }
 
 function resetGame() {
-  guessesLeft = 10;
+  guessesLeft = 8;
   lettersTried = [];
   activeWord = wordBank[Math.floor(Math.random() * wordBank.length)];
   guessArray = [];
