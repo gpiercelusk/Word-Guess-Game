6ed8656ac84ea672b502b506f96e00f75f2270ec
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-02-24 17:23:23 -0500

    learned new things complete rework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,133 +1,137 @@
-var wordBank =
-  [
-    "sourdough", "brioche", "marbled rye", "cornbread", "ciabatta", "focaccia", "lavash", "pumpernickel", "baguette", "boule", "dampfnudel",
-  ];
-
-
-//letters tried
-var lettersTried = [];
-//index of chosen word form wordbank
-var activeWordIndex;
-//array of the active word
-var activeWord;
-// guesses left
-var guessesLeft = 10;
-var guessArray = [];
-var gameStart = false;
-var finish = false;
+var wordBank = ["sourdough", "brioche", "marbled rye", "cornbread", "ciabatta", "focaccia", "lavash", "pumpernickel", "baguette", "boule", "dampfnudel", "yeast", "flour", "sugar", "gluten", "water", "salt"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
 var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var lettersTried = [];
+var userGuesses = null;
+var activeWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+var guessArray = [];
+var html = "<p><h1>";
 
-
-function gameRestart() {
-  guessesLeft = 10;
-  activeWordIndex = Math.floor(Math.random() * (wordBank.length));
-  lettersTried = [];
-  activeWord = wordBank[activeWordIndex].split("");
-  document.addEventListener("keydown", getUserGuess);
-  initGuessArray()
-};
-
-function initGuessArray() {
-  for (var i = 0; i < activeWord.length; i++) {
-    var letterObject = {
-      letter: activeWord[i],
-      isGuessed: false
+// Turn word into array
+// Create a loop for array and add true false tags
+function splitUpWord() {
+  for (var i = 0, g = 0; i < activeWord.length; i++) {
+    guessArray[g] = activeWord.charAt(i);
+    g++
+    if (activeWord.charAt(i) != " ") {
+      guessArray[g] = false;
+    } else {
+      guessArray[g] = true;
     }
-
-    guessArray.push(letterObject);
+    g++
   }
 }
 
-function getUserGuess(event) {
-  if (event.keyCode >= 65 && event.keyCode <= 90) {
-    // repeat key press indication 
-    if (lettersTried.includes(event.key)) {
-      console.log("poopFace")
+function consoleLogger() {
+  console.log("wins: " + wins + "\n" + "losses: " + losses + "\n");
+  console.log("guessesLeft: " + guessesLeft + "\n");
+  console.log("lettersTried: " + lettersTried + "\n");
+  console.log("activeWord: " + activeWord + "\n");
+  console.log("guessArray: " + guessArray + "\n");
+  console.log("--------------------------------");
+}
+
+function resetGame() {
+  guessesLeft = 10;
+  lettersTried = [];
+  activeWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+  guessArray = [];
+  splitUpWord();
+  var instructions = "<p><h3>Press any key to start the game</p></h3>";
+  document.querySelector("#instructions").innerHTML = instructions;
+  var initGame = "<p><h1>";
+  for (var i = 0; i < activeWord.length; i++) {
+    if (activeWord.charAt(i) == " ") {
+      initGame += "&nbsp;&nbsp;";
     } else {
-      // letters tried push into previously guessed array
-      lettersTried.push(event.key.toLowerCase())
-      guessTest(event.key.toLowerCase());
+      initGame += "_&nbsp;";
     }
   }
-};
+  initGame += "</h1></p>"
+  document.querySelector("#game").innerHTML = initGame;
+  var htmlStats = "<p><h3>" + "Wins: " + wins + " Losses: " + losses + " Guesses Left : " + guessesLeft + "</h3></p>";
+  document.querySelector("#stats").innerHTML = htmlStats;
+}
 
-
-function guessTest(letter) {
-  // return an array of letters from the guess array 
-  var arr = guessArray.map(item => item.letter)
-  console.log(arr)
-  if (arr.includes(letter)) {
-    for (var i = 0; i < guessArray.length; i++) {
-      console.log(guessArray[i])
-      console.log(lettersTried)
-      if (guessArray[i].letter === letter) {
-        guessArray[i].isGuessed = true
-        console.log(guessArray[i].isGuessed)
-      }
+//Update on screen game, stats, and guessed letters
+function howYouDoin() {
+  for (i = 0, g = 0; i < (guessArray.length / 2); i++) {
+    if (guessArray[g + 1] == true) {
+      html += guessArray[g];
+    } else {
+      html += "_";
     }
-  } else {
-    guessesLeft--
-    console.log(guessesLeft)
+    html += "&nbsp;";
+    g = g + 2;
   }
+  html += "</h1></p>"
+  document.querySelector("#game").innerHTML = html;
+  htmlStats = "<p><h3>Wins: " + wins + " Losses: " + losses + " Guesses Left : " + guessesLeft + "</h3></p>";
+  document.querySelector("#stats").innerHTML = htmlStats;
+  htmlGuesses = "<p><h3>"
+  for (var i = 0; i < lettersTried.length; i++) {
+    htmlGuesses += lettersTried[i] + "&nbsp;";
+  }
+  htmlGuesses += "</h3></p>";
+  document.querySelector("#guesses").innerHTML = htmlGuesses;
+}
 
-};
-
-gameRestart();
-
-
-
-// document.onkeydown = function (event) {
-//   if (finish) {
-//     gameRestart();
-//     finish = false;
-//   } else {
-//    
-//   }
-// };
-
-// function guessSelect(letter) {
-//   if (guessesLeft > 0) {
-//     if (!gameStarted) {
-//       gameStarted = true;
-//     }
-
-//     if (lettersTried.indexOf(letter) === -1) {
-//       lettersTried.push(letter);
-//       guessTest(letter);
-//     }
-//   }
-
-//   updateDisplay();
-//   checkWin();
-// };
+//check if guess is valid
+//if false change to true
+function checkGuess() {
+  if (guessArray.indexOf(userGuess) < 0 && lettersTried.indexOf(userGuess) < 0 && letters.indexOf(userGuess) >= 0) {
+    guessesLeft--;
+  }
+  if (lettersTried.indexOf(userGuess) < 0 && letters.indexOf(userGuess) >= 0) {
+    lettersTried[lettersTried.length] = userGuess;
+  }
+  for (var i = 0; i < guessArray.length; i++) {
+    if (guessArray[i] === userGuess) {
+      guessArray[i + 1] = true;
+    }
+  }
+}
 
 
-// function guessTest(letter) {
-//   var letterStorage = [];
+function winCheck() {
+  if (guessArray.indexOf(false) < 0) {
+    alert("MMMMM Yummy! Nom Nom!");
+    wins++;
+    resetGame();
+  }
+}
 
-//   for (var i = 0; i < wordBank[activeWordIndex].length; i++) {
-//     if (wordBank[activeWordIndex][i] === letter) {
-//       letterStorage.push(i);
-//     }
-//   }
+
+function lossesCheck() {
+  if (guessesLeft == 0) {
+    alert("NO BREAD FOR YOU!");
+    losses++;
+    resetGame();
+  }
+}
 
 
-//   if (letterStorage.length <= 0) {
-//     guessesLeft--;
+function htmlVar() {
+  html = "<p><h1>";
+}
 
-//   } else {
+// Set up game
+splitUpWord();
+resetGame();
+consoleLogger();
+// run events
+document.onkeyup = function (event) {
+  userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+  checkGuess();
+  howYouDoin();
+  htmlVar();
+  winCheck();
+  lossesCheck();
+  consoleLogger();
+}
 
-//     for (var i = 0; i < letterStorage.length; i++) {
-//       activeWord[letterStorage[i]] = letter;
-//     }
-//   }
-// };
 
-// function checkWin() {
-//   if (activeWord.indexOf("_") === -1) {
-//     document.getElementById("winner-image").style.cssText = "display: block";
-//     document.getElementById("pressKeyTryAgain").style.cssText = "display: block";
-//     wins++;
-//     finish = true;
-//   }
-// };
+
+
